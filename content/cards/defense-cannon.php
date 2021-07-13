17eb3dc53adb4f5a63a40b774063eea753b4849a
Jim Westergren <jim@jimwestergren.com>
2021-07-13 19:45:29 +0200

    Buff to Defense Cannon

--- content/cards/defense-cannon.php
+++ content/cards/defense-cannon.php
@@ -1,7 +1,21 @@
 <p>Defense Cannon is great for attacking those enemy spaceships in your home zone.</p>
 
+<h2>Good targets</h2>
+
+<?=cardImage('trigger-happy-joe')?>
+<?=cardImage('the-gladiator')?>
+<?=cardImage('hunter')?>
+<?=cardImage('drone')?>
+<?=cardImage('alien-missile')?>
+<?=cardImage('pursuiter')?>
+
+<h2>Good against Defense Cannon</h2>
+
+<?=cardImage('acid-projectile')?>
+
 <h2>Changelog During Playtesting</h2>
 
 <ul>
+    <li>Buff in July 2021: Earlier a simple 4/1 that could not move.</li>
     <li>September 2018: Created.</li>
 </ul>
