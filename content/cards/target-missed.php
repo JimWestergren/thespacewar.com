8fc225c07751d75e40c5543342fcf4bd339ca0e7
Jim Westergren <jim@jimwestergren.com>
2021-07-13 19:45:12 +0200

    Buff to Target Missed

--- content/cards/target-missed.php
+++ content/cards/target-missed.php
@@ -1,4 +1,15 @@
-<p>Crucial card when you need it. Excellent to be kept as a station card for later use.</p>
+<p>Great card against both missiles and big spaceships. Excellent to be kept as a station card for later use.</p>
+
+
+<h2>Examples of missiles that it stops</h2>
+
+<?=cardImage('death-ray')?>
+<?=cardImage('nuclear-missile')?>
+<?=cardImage('explosive-missile')?>
+<?=cardImage('fast-missile')?>
+<?=cardImage('cluster-nuke')?>
+<?=cardImage('alien-missile')?>
+
 
 <h2>Spaceships with high attack value</h2>
 
@@ -10,10 +21,13 @@
 <h2>Cards good against this</h2>
 
 <?=cardImage('destiny-decided')?>
+<?=cardImage('time-warp')?>
 <?=cardImage('luck')?>
+<?=cardImage('fate')?>
 
 <h2>Changelog During Playtesting</h2>
 
 <ul>
+    <li>Buff in July 2021: Can now cancel any attack, not only from spaceships.</li>
     <li>September 2018: Created.</li>
 </ul>
