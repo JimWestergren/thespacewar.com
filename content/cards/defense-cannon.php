2e39152201dfd59a08c4845637a166277d026a95
Jim Westergren <jim@jimwestergren.com>
2021-10-01 11:45:39 +0200

    Buff to Defense Cannon

--- content/cards/defense-cannon.php
+++ content/cards/defense-cannon.php
@@ -16,6 +16,7 @@
 <h2>Changelog During Playtesting</h2>
 
 <ul>
+    <li>Buff in September 2021: Now cannot be attacked if you have a shield.</li>
     <li>Buff in July 2021: Earlier a simple 4/1 that could not move.</li>
     <li>September 2018: Created.</li>
 </ul>
