3207c85edcbc7090eaf0dff923b7ab33680cb28d
Jim Westergren <jim@jimwestergren.com>
2025-08-09 20:17:53 +0200

    Card Discovery nerfed

--- content/cards/sacrifice.php
+++ content/cards/sacrifice.php
@@ -1,7 +1,6 @@
 <h2>When is this card great?</h2>
 
 <ul>
-    <li>You play with a deck that seldom has cards in play.</li>
     <li>You play against a deck that has few but big cards to put in play.</li>
     <li>When you can use it to remove a big Shield.</li>
 </ul>
@@ -13,6 +12,7 @@
 <h2>Changelog During Playtesting</h2>
 
 <ul>
+    <li>Nerf in August 2024:<br>Now both players need to discard from play.</li>
     <li>Buff in 2020:<br>"if they have" was added making the card powerful when you don't have any cards in play.</li>
     <li>September 2019: Created.</li>
 </ul>
