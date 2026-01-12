91e749603eccf416bb40f7b05850a59724a6e82d
Jim Westergren <jim@jimwestergren.com>
2026-01-12 10:42:37 +0100

    Improving the lore text on the commander cards

--- view/commander.php
+++ view/commander.php
@@ -23,11 +23,6 @@ require(ROOT.'view/head.php');
 
 <div class="lore"><?=$data['lore']?></div>
 
-<?php if ($data['name'] == 'Nicia Satu') { ?>
-    <h2>Rule Clarification</h2>
-    <p>Commander is always selected directly after drawing the start hand. If you choose a green defense card that is in your hand, put it on the table and draw a new card. Otherwise select a defense card from your draw pile and then shuffle your draw pile.</p>
-<?php } ?>
-
 
 <?//=displayCardNFTinfo('commander-'.$slug)?>
 
