91da64094caa015ce7ab8cab66e0f3fe0e145a8a
Jim Westergren <jim@jimwestergren.com>
2026-01-13 09:43:14 +0100

    Buff to Hacker

--- content/cards/hacker.php
+++ content/cards/hacker.php
@@ -8,15 +8,10 @@
 <?=cardImage('death-ray-cannon')?>
 <?=cardImage('time-travel-portal')?>
 
-<h2>How to Easy get rid of Hacker</h2>
-
-<?=cardImage('hunter')?>
-<?=cardImage('ambush')?>
-<?=cardImage('the-gladiator')?>
-
 <h2>Changelog During Playtesting</h2>
 
 <ul>
+    <li>Buff in January 2026: Defense value increased from 3 to 4 (prevent being destroyed by Ambush).</li>
     <li>March 2023: Card simplified for more easy understanding. Now you can take either spaceship or missile but it is only until end of turn.</li>
     <li>April 2020: Created.</li>
 </ul>
