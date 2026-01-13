fc0e0a5d68d13b9854df569b781f096af519a675
Jim Westergren <jim@jimwestergren.com>
2026-01-13 09:43:42 +0100

    Buff to Supernova, prevent draws

--- content/cards/supernova.php
+++ content/cards/supernova.php
@@ -13,12 +13,13 @@
 <h2>Rule Clarifications</h2>
 
 <ul>
-    <li>You are not allowed to play Supernova if you your station has 3 or less remaining life.</li>
+    <li>Supernova cannot cause a draw as if you use this to kill the commander of the opponent you win the game before you get the damage.</li>
 </ul>
 
 <h2>Changelog During Playtesting</h2>
 
 <ul>
+    <li>Buff in January 2026: Damage to opponent station is dealt first, draw is prevented.</li>
     <li>Buff in December 2025: Cost to play is reduced from 8 to 7.</li>
     <li>September 2018: Created.</li>
 </ul>
