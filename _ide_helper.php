ec3c82facac77c1a333a24a228215b14350180d8
Jim Westergren <jim@jimwestergren.com>
2026-03-06 00:02:11 +0100

    Create _ide_helper.php

---
+++ _ide_helper.php
@@ -0,0 +1,15 @@
+<?php
+// This file is only to help the IDE resolve constants defined outside the workspace.
+// It is never included or executed by the application.
+define('SECRET_MYSQL_HOST', '');
+define('SECRET_MYSQL_DATABASE', '');
+define('SECRET_MYSQL_USERNAME', '');
+define('SECRET_MYSQL_PASSWORD', '');
+define('SECRET_SALT_LOG_GAME', '');
+define('SECRET_SALT_LOGIN_COOKIE', '');
+define('SECRET_SALT_PASSWORD_RESET', '');
+define('SECRET_SALT_VALIDATE_EMAIL', '');
+define('SECRET_SALT_NFT_CODE', '');
+define('SECRET_OPENSEA_API_KEY', '');
+define('SECRET_DEBUG_EMAIL', '');
+define('SECRET_SERVER_IP_ARRAY', []);
